fe88a7598422e92ce43f6b69c49871a088903ad4
technophile22 <raghavmangal22@gmail.com>
2020-07-30 14:15:13 +0530

    Designed scream cards

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 //Components
 import Navbar from './components/Navbar';
 //Pages
@@ -9,9 +10,31 @@ import home from './pages/home';
 import login from './pages/login';
 import signup from './pages/signup';
 
+
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        light: '#33c9dc',
+        main: '#1da1f2',
+        dark: '#008394',
+        contrastText: '#fff'
+      },
+      secondary: {
+        light: '#ff6333',
+        main: '#ff3d00',
+        dark: '#b22a00',
+        contrastText: '#fff'
+      }
+    },
+    typography: {
+      useNextVariants: true
+    }
+});
+
 function App() {
   return (
-    <div className="App">
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
       <Router>
       <Navbar/>
         <div className="container">
@@ -23,6 +46,7 @@ function App() {
         </div>
       </Router>
     </div>
+    </MuiThemeProvider>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,17 +6,17 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
-
 class Navbar extends Component {
     render() {
         return (
             <AppBar>
                 <Toolbar className= "nav-container">
+                
                     <Button color="inherit" component = {Link} to="/login">Login</Button>
                     <Button color="inherit" component = {Link} to="/">Home</Button>
                     <Button color="inherit" component = {Link} to="/signup">Signup</Button>
+                    
                 </Toolbar>
-
             </AppBar>
         )
     }
