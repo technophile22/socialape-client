c2e800c11595e18d482f2efa773f8c18078733bb
technophile22 <raghavmangal22@gmail.com>
2020-08-07 19:54:59 +0530

    Posting a scream feature is implemented

--- src/redux/reducers/dataReducer.js
+++ src/redux/reducers/dataReducer.js
@@ -1,4 +1,4 @@
-import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM} from '../types';
+import { SET_SCREAMS, LIKE_SCREAM, UNLIKE_SCREAM, LOADING_DATA, DELETE_SCREAM, POST_SCREAM} from '../types';
 
 const initialState = {
     screams: [],
@@ -35,6 +35,14 @@ export default function(state = initialState, action){
                 return {
                     ...state
                 };
+            case POST_SCREAM:
+                return {
+                    ...state,
+                    screams: [
+                        action.payload, 
+                        ...state.screams
+                    ]
+                }
         default:
             return state
     }
