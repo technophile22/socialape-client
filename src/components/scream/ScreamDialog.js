bfe82c6e308485fb2b3403d546182f50a7a61b9d
technophile22 <raghavmangal22@gmail.com>
2020-08-08 12:19:00 +0530

    Formatted the structure of files for better understanding of code

--- src/components/scream/ScreamDialog.js
+++ src/components/scream/ScreamDialog.js
@@ -1,7 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import withStyles from '@material-ui/core/styles/withStyles';
-import MyButton from '../util/MyButton';
+import MyButton from '../../util/MyButton';
 import dayjs from 'dayjs';
 import {Link} from 'react-router-dom';
 import LikeButton from './LikeButton';
@@ -19,7 +19,7 @@ import ChatIcon from '@material-ui/icons/Chat';
 
 //redux stuff
 import { connect } from 'react-redux';
-import { getScream } from '../redux/actions/dataActions';
+import { getScream } from '../../redux/actions/dataActions';
 
 
 const styles =  (theme) => ({
