bc0aa63c1d4f5961b8c88e7a6aae8460c18376a8
technophile22 <raghavmangal22@gmail.com>
2020-08-09 22:43:24 +0530

    User page designed

--- src/redux/actions/dataActions.js
+++ src/redux/actions/dataActions.js
@@ -104,6 +104,24 @@ export const deleteScream = (screamId) => (dispatch) => {
         .catch( err => console.log(err));
 };
 
+//for user page
+export const getUserData = (userHandle) => (dispatch) => {
+    dispatch({ type: LOADING_DATA });
+    axios.get(`/user/${userHandle}`)
+        .then(res => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: res.data.screams
+            });
+        })
+        .catch( () => {
+            dispatch({
+                type: SET_SCREAMS,
+                payload: null
+            })
+        })
+}
+
 export const clearErrors = () => (dispatch) => {
     dispatch({ type: CLEAR_ERRORS});
 }
