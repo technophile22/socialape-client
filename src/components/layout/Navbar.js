13b766c22894e3d01870e6decc9d32192b8ac55b
technophile22 <raghavmangal22@gmail.com>
2020-08-10 16:02:38 +0530

    Notifications part implemented

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 import Proptypes from 'prop-types';
 import MyButton from '../../util/MyButton';
 import PostScream from '../scream/PostScream';
+import Notifications from './Notifications';
+
 //MUI stuff
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -11,7 +13,6 @@ import Button from '@material-ui/core/Button';
 
 //Icons
 import HomeIcon from '@material-ui/icons/Home';
-import Notifications from '@material-ui/icons/Notifications';
 
 class Navbar extends Component {
     render() {
@@ -26,10 +27,8 @@ class Navbar extends Component {
                             <MyButton tip="Home">
                                 <HomeIcon />
                             </MyButton>
-                            </Link>
-                            <MyButton tip="Notifications">
-                                <Notifications  />
-                            </MyButton>
+                            </Link>             
+                                <Notifications />
                         </Fragment>
 
                  ) : (
